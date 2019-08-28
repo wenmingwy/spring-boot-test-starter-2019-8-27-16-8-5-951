0dcec3906709f69b10efab9a50286f549e5292ae
wenmingwy <2377812120@qq.com>
2019-08-28 17:27:02 +0800

    final

--- src/main/java/com/oocl/web/sampleWebApp/HomeController.java
+++ src/main/java/com/oocl/web/sampleWebApp/HomeController.java
@@ -1,8 +1,13 @@
 package com.oocl.web.sampleWebApp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
@@ -11,4 +16,23 @@ public class HomeController {
     public @ResponseBody String greeting() {
         return "Hello World";
     }
+    
+    @RequestMapping("/hello")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody String greeting1() {
+        return "Hello World";
+    }
+  @RequestMapping("/user")
+    public @ResponseBody Map MapgetName() {
+    	Map user = new HashMap();
+    	user.put("user","name");
+    	return user;
+    }
+  
+  @RequestMapping("/user")
+  public @ResponseBody Map Mapjson() {
+  	Map user = new HashMap();
+  	user.put("id","001");
+  	return user;
+  }
 }
--- src/test/java/com/oocl/web/sampleWebApp/ApplicationTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/ApplicationTest.java
@@ -6,13 +6,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,4 +35,56 @@ public class ApplicationTest {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello World")));
     }
+    
+    @Test
+    public void shouldReturnDefaultMessage1() throws Exception {
+        this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+    
+    @Test
+    public void shouldGotNameInBody() throws Exception {
+        this.mockMvc.perform(
+        		get("/user")
+        )
+        .andDo(print())
+        .andExpect(
+        		status().isOk()
+        )
+        .andExpect(
+        		MockMvcResultMatchers.jsonPath(
+        				"$.user",
+        				CoreMatchers.is("name")
+        		)
+//        		content().string(containsString("Hello World")));
+        );
+    }
+    
+    
+    @Autowired
+    private ObjectMapper objectMapper ;
+    
+    @Test
+    public void shouldGotGeneratedInBody() throws Exception {
+    	HashMap map = new HashMap();
+    	String postString = objectMapper.writeValueAsString(map) ;
+        this.mockMvc.perform(
+        		MockMvcRequestBuilders
+        		.post("/users")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(postString)
+        )
+        .andDo(print())
+        .andExpect(
+        		status().()
+        )
+        .andExpect(
+        		MockMvcResultMatchers.jsonPath(
+        				"$.id",
+        				CoreMatchers.is("001")
+        		)
+        );
+    } 
+    
+    
 }
